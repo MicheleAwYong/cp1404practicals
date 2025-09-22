01d4357481cc552448aac794beea73c8b9952eb7
MicheleAwYong <micheleeehuan.awyong@my.jcu.edu.au>
2025-09-22 18:39:43 +0800

    Removed some text

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,17 +1,14 @@
 MINIMUM_LENGTH = 4
 def main():
-    """Get and check password, then print asterisks."""
     password = get_password()
     print_asterisks(password)
 def get_password():
-    """Get a password of valid length from the user."""
     password = input("Enter password: ")
     while len(password) < MINIMUM_LENGTH:
         print("Password is too short.")
         password = input("Enter password: ")
     return password
 def print_asterisks(password):
-    """Print asterisks equal to the length of the password."""
     print('*' * len(password))
 main()
 "This is for the Github"
