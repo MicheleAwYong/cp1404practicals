107eebdcf45d28f74e7ad43107d5c55e5941d64f
MicheleAwYong <micheleeehuan.awyong@my.jcu.edu.au>
2025-09-29 09:30:51 +0800

    Ran the code multiple times to determine what was the smallest and the largest number

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -3,3 +3,7 @@ import random
 print(random.randint(5, 20))  # line 1
 print(random.randrange(3, 10, 2))  # line 2
 print(random.uniform(2.5, 5.5))  # line 3
+
+#for line 1 the small number I saw was 5 and the largest was 20
+#line 2 the smallest was 3 and the largest was 9
+#line 3 the smallest was 2.606 and the largest was 4.8264
