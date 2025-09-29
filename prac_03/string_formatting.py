cef8783950a4ededfab09f1447f1369c519817cd
MicheleAwYong <micheleeehuan.awyong@my.jcu.edu.au>
2025-09-29 09:04:37 +0800

    Trying out different combinations

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -1,5 +1,11 @@
 name = "Gibson L-5 CES"
 year = 1922
-cost = 16035.405
+cost = 16035.9
+
 
 print("My guitar: " + name + ", first made in " + str(year))
+print("My guitar: {}, first made in {}".format(name, year))
+print("My guitar: {0}, first made in {1}".format(name, year))
+print("My {0} was first made in {1} (that's right, {1}!)".format(name, year))
+
+
