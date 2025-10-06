ab5fcb7e4ef9c27eb0f9c94d09e8fdf265289e7e
MicheleAwYong <micheleeehuan.awyong@my.jcu.edu.au>
2025-10-06 17:12:41 +0800

    Added new function to display all of the subject details

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -4,10 +4,10 @@ Data file -> lists program
 """
 FILENAME = "subject_data.txt"
 
-
 def main():
     data = load_data(FILENAME)
-    print(data)
+    display_subject_details(data)
+
 
 def load_data(filename=FILENAME):
     all_subject_data = []
@@ -22,9 +22,17 @@ def load_data(filename=FILENAME):
             except ValueError:
                 print(f"Error: Could not convert student number for line: {line}")
                 continue
-
             all_subject_data.append(parts)
-
     return all_subject_data
+
+def display_subject_details(data):
+    print("\n--- Subject Details ---")
+    for subject in data:
+        subject_code = subject[0]
+        lecturer_name = subject[1]
+        num_students = subject[2]
+
+        print(f"{subject_code} is taught by {lecturer_name:<15} and has {num_students:>3} students")
+
 
 main()
