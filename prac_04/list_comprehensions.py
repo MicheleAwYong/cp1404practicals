df9d83978c3d101e5e2a19e084d3a8284bce90da
MicheleAwYong <micheleeehuan.awyong@my.jcu.edu.au>
2025-10-06 17:49:29 +0800

    Added for loop that creates list for first letter

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -7,10 +7,8 @@ names = ["Bob", "Angel", "Jimi", "Alan", "Ada"]
 full_names = ["Bob Martin", "Angel Harlem", "Jimi Hendrix", "Alan Turing", "Ada Lovelace"]
 
 # for loop that creates a new list containing the first letter of each name
-first_initials = []
-for name in names:
-    first_initials.append(name[0])
-print(first_initials)
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)
 
 # list comprehension that does the same thing as the loop above
 first_initials = [name[0] for name in names]
