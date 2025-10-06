3b1fb7eda9e659430909bd6aadde5c48a7d6c8ff
MicheleAwYong <micheleeehuan.awyong@my.jcu.edu.au>
2025-10-06 17:50:55 +0800

    Added for loop that creates list for first letter and fixed my mistake

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -7,8 +7,10 @@ names = ["Bob", "Angel", "Jimi", "Alan", "Ada"]
 full_names = ["Bob Martin", "Angel Harlem", "Jimi Hendrix", "Alan Turing", "Ada Lovelace"]
 
 # for loop that creates a new list containing the first letter of each name
-lowercase_full_names = [name.lower() for name in full_names]
-print(lowercase_full_names)
+first_initials = []
+for name in names:
+    first_initials.append(name[0])
+print(first_initials)
 
 # list comprehension that does the same thing as the loop above
 first_initials = [name[0] for name in names]
@@ -28,7 +30,8 @@ print(a_names)
 print(" ".join(sorted(names)))
 
 # TODO: list comprehension to create a list of all the full_names in lowercase format
-# lowercase_full_names =
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: list comprehension to create a list of integers from the above list of strings
