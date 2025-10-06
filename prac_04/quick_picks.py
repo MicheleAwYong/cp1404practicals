1f59d353481e08c1758e303c667125d9e5ebe74b
MicheleAwYong <micheleeehuan.awyong@my.jcu.edu.au>
2025-10-06 18:22:08 +0800

    I referred to the solution and realized I had written it wrongly.

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,22 @@
+import random
+
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+NUMBERS_PER_PICK = 6
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    while number_of_quick_picks < 0:
+        print("That makes no sense!")
+        number_of_quick_picks = int(input("How many quick picks? "))
+
+    for i in range(number_of_quick_picks):
+        quick_pick = []
+        for j in range(NUMBERS_PER_PICK):
+            number = random.randint(MIN_NUMBER, MAX_NUMBER)
+            while number in quick_pick:
+                number = random.randint(MIN_NUMBER, MAX_NUMBER)
+            quick_pick.append(number)
+        quick_pick.sort()
+        print(" ".join(f"{number:2}" for number in quick_pick))
+main()
