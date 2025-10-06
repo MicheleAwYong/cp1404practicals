df5472375cc2c67b7753ed431db728701b227f83
MicheleAwYong <micheleeehuan.awyong@my.jcu.edu.au>
2025-10-06 16:37:55 +0800

    Using an f-string instead of string concatenation (+)

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -10,7 +10,7 @@ def main():
     months = int(input("How many months? "))
 
     for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+        income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
@@ -22,3 +22,15 @@ def main():
 
 
 main()
+
+#How do you add a list?
+#income = []
+
+#We need a counter variable (int) for the month number. Remember that list indexes start at 0, but we want to print from 1.
+#income = incomes[month - 1]
+
+#How many loops will we need? What kind of loops?
+#We need two loops and they both have to be for loops.
+
+#We need a cumulative total to update as we loop through the list to display the incomes.
+#total = 0 inside the first for loop and total += income for the second one
