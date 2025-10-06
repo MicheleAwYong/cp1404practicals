3f68aa20f614e7d6d12a118e17d9e9d513a61596
MicheleAwYong <micheleeehuan.awyong@my.jcu.edu.au>
2025-10-06 16:50:59 +0800

    Unpdated with Refactored code for printing section

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -2,8 +2,6 @@
 CP1404/CP5632 Practical
 Starter code for cumulative total income program
 """
-
-
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
@@ -13,15 +11,19 @@ def main():
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
+    # Call the new function to print the report
+    print_report(incomes, num_months)
+
+
+def print_report(incomes, num_months):
     print("\nIncome Report\n-------------")
     total = 0
+
     for month in range(1, num_months + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
-
-main()
 
 main()
 
