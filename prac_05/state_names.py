51fa1dfc2e65808b372d424a74156ddad2bf5ac9
MicheleAwYong <micheleeehuan.awyong@my.jcu.edu.au>
2025-10-13 09:36:20 +0800

    Added the new state code to name list

---
+++ prac_05/state_names.py
@@ -0,0 +1,19 @@
+STATE_CODES_TO_NAMES = {
+    "QLD": "Queensland",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "WA": "Western Australia",
+    "ACT": "Australian Capital Territory",
+    "VIC": "Victoria",
+    "TAS": "Tasmania",
+    "SA": "South Australia",
+}
+print(STATE_CODES_TO_NAMES)
+
+state_code = input("Enter short state: ")
+while state_code != "":
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    else:
+        print("Invalid short state")
+    state_code = input("Enter short state: ")
